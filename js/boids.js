0f47f3e002c49e45c64af4038379563ab1c7244e
Taybin Rutkin <taybin@taybin.com>
2017-04-01 19:09:09 -0400

    fix indentation

--- js/boids.js
+++ js/boids.js
@@ -6,31 +6,31 @@ window.addEventListener("DOMContentLoaded", function(evt) {
   var canvas = document.getElementById('renderCanvas');
   var engine = new BABYLON.Engine(canvas, true);
 
-	var createScene = function() {
-			// create a basic BJS Scene object
-			var scene = new BABYLON.Scene(engine);
+  var createScene = function() {
+    // create a basic BJS Scene object
+    var scene = new BABYLON.Scene(engine);
 
-			// create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-			var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -500), scene);
+    // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -500), scene);
 
-			// target the camera to scene origin
-			camera.setTarget(BABYLON.Vector3.Zero());
+    // target the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
 
-			// attach the camera to the canvas
-			camera.attachControl(canvas, false);
+    // attach the camera to the canvas
+    camera.attachControl(canvas, false);
 
-			// create a basic light, aiming 0,1,0 - meaning, to the sky
-			var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+    // create a basic light, aiming 0,1,0 - meaning, to the sky
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
-			// return the created scene
-			return scene;
-	};
+    // return the created scene
+    return scene;
+  };
 
-	var scene = createScene();
+  var scene = createScene();
 
-	engine.runRenderLoop(function() {
-		scene.render();
-	});
+  engine.runRenderLoop(function() {
+    scene.render();
+  });
 
   window.addEventListener('resize', function() {
     engine.resize();
