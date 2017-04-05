ab2156802637e276505d302c3ff67ecda87c27cc
Taybin Rutkin <trutkin@novetta.com>
2017-04-05 13:50:46 -0400

    refactor web stuff into web.go

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/gorilla/websocket"
-	"log"
-	"net/http"
 	"time"
 )
 
 type appContext struct {
 	area *Area
 }
-
-var lastID = 0
-var upgrader = websocket.Upgrader{} // use default options
 
 func main() {
 	context := &appContext{
@@ -25,6 +18,8 @@ func main() {
 }
 
 func setupBoids(context *appContext) {
+	var lastID = 0
+
 	for i := 0; i < 500; i++ {
 		lastID = lastID + 1
 		boid := NewBoid(lastID, len(context.area.Dimensions))
@@ -38,46 +33,4 @@ func setupBoids(context *appContext) {
 			time.Sleep(time.Second * 4)
 		}
 	}()
-}
-
-type appHandler struct {
-	*appContext
-	H func(*appContext, http.ResponseWriter, *http.Request)
-}
-
-func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ah.H(ah.appContext, w, r)
-}
-
-func setupWeb(context *appContext) {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
-	http.Handle("/ws", appHandler{context, websockHandler})
-
-	log.Println("Listening...")
-	http.ListenAndServe(":3000", nil)
-}
-
-func websockHandler(context *appContext, w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-	for {
-		select {
-		case boid := <-context.area.SendChan:
-			jBoid, err := json.Marshal(boid)
-			if err != nil {
-				log.Println("error encoding boid:", err)
-				return
-			}
-			err = c.WriteMessage(websocket.TextMessage, jBoid)
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
-		}
-	}
 }
---
+++ web.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+type appHandler struct {
+	*appContext
+	H func(*appContext, http.ResponseWriter, *http.Request)
+}
+
+func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ah.H(ah.appContext, w, r)
+}
+
+func setupWeb(context *appContext) {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.Handle("/ws", appHandler{context, websockHandler})
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+}
+
+func websockHandler(context *appContext, w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		select {
+		case boid := <-context.area.SendChan:
+			jBoid, err := json.Marshal(boid)
+			if err != nil {
+				log.Println("error encoding boid:", err)
+				return
+			}
+			err = c.WriteMessage(websocket.TextMessage, jBoid)
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		}
+	}
+}
